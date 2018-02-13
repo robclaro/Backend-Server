5e4e976dd72aaa39d6521bd182bf6052c454d342
Robert Claro <rclaro@Roberts-MacBook-Pro-3.local>
2018-02-13 11:29:53 -0500

    Secc. 09 Curso Angular terminada

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-// Requires 
+// Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-
-// inicializar variables
-
+// Inicializar variables
 var app = express();
 
-// conexion a la base de datos
-mongoose.connect('mongodb://localhost:27017/hospitalDB', (err, res)=>{
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
+// Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+
     if (err) throw err;
-    console.log('Conexión a la Base de Datos : \x1b[32m%s\x1b[0m',' online');
+
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+
 });
 
 
 // Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
-app.get('/', (req, res, next) =>
-{
-    res.status(200).json({
-        ok: true,
-        mensaje: 'petición realizada correctamente'
-    })
-}
-)
 
 // Escuchar peticiones
-
-app.listen(3000, ()=>{
-    // color verde en la consola  \x1b[32m%s\x1b[0m
-    console.log('Express Serve corriendo puerto 3000 : \x1b[32m%s\x1b[0m',' online');
+app.listen(3000, () => {
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
 });
