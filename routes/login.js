d64a7ac75ff1c691fcbcccb2dbb68fd255dc8c84
Robert Claro <rclaro@Roberts-MacBook-Pro-3.local>
2018-02-13 21:07:44 -0500

    Secc. 18 Terminada del Backend - Sever

--- routes/login.js
+++ routes/login.js
@@ -14,6 +14,23 @@ var auth = new GoogleAuth;
 const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
 const GOOGLE_SECRET = require('../config/config').GOOGLE_SECRET;
 
+
+var mdAutenticacion = require('../middlewares/autenticacion');
+
+// ==========================================
+//  Autenticación De Google
+// ==========================================
+app.get('/renuevatoken', mdAutenticacion.verificaToken, (req, res) => {
+
+    var token = jwt.sign({ usuario: req.usuario }, SEED, { expiresIn: 14400 }); // 4 horas
+
+    res.status(200).json({
+        ok: true,
+        token: token
+    });
+
+});
+
 // ==========================================
 //  Autenticación De Google
 // ==========================================
@@ -72,7 +89,8 @@ app.post('/google', (req, res) => {
                             ok: true,
                             usuario: usuario,
                             token: token,
-                            id: usuario._id
+                            id: usuario._id,
+                            menu: obtenerMenu(usuario.role)
                         });
 
                     }
@@ -106,7 +124,8 @@ app.post('/google', (req, res) => {
                             ok: true,
                             usuario: usuarioDB,
                             token: token,
-                            id: usuarioDB._id
+                            id: usuarioDB._id,
+                            menu: obtenerMenu(usuarioDB.role)
                         });
 
                     });
@@ -166,7 +185,8 @@ app.post('/', (req, res) => {
             ok: true,
             usuario: usuarioDB,
             token: token,
-            id: usuarioDB._id
+            id: usuarioDB._id,
+            menu: obtenerMenu(usuarioDB.role)
         });
 
     })
@@ -175,6 +195,41 @@ app.post('/', (req, res) => {
 });
 
 
+
+function obtenerMenu(ROLE) {
+
+    var menu = [{
+            titulo: 'Principal',
+            icono: 'mdi mdi-gauge',
+            submenu: [
+                { titulo: 'Dashboard', url: '/dashboard' },
+                { titulo: 'ProgressBar', url: '/progress' },
+                { titulo: 'Gráficas', url: '/graficas1' },
+                { titulo: 'Promesas', url: '/promesas' },
+                { titulo: 'RxJs', url: '/rxjs' }
+            ]
+        },
+        {
+            titulo: 'Mantenimientos',
+            icono: 'mdi mdi-folder-lock-open',
+            submenu: [
+                // { titulo: 'Usuarios', url: '/usuarios' },
+                { titulo: 'Hospitales', url: '/hospitales' },
+                { titulo: 'Médicos', url: '/medicos' }
+            ]
+        }
+    ];
+
+    console.log('ROLE', ROLE);
+
+    if (ROLE === 'ADMIN_ROLE') {
+        menu[1].submenu.unshift({ titulo: 'Usuarios', url: '/usuarios' });
+    }
+
+
+    return menu;
+
+}
 
 
 
